fcdd17bc54ea050f934a9b3f82a5dba6425fe937
zubenkom249 <zubenkom249@gmail.com>
2025-03-18 16:33:48 +0200

    Add files via upload

---
+++ GuessNumberGame/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace GuessNumberGame
+{
+    public class NumberGame
+    {
+        private readonly int _targetNumber;
+        private int _attempts;
+
+        public NumberGame()
+        {
+            Random random = new Random();
+            _targetNumber = random.Next(1, 101);
+            _attempts = 0;
+        }
+
+        public int Attempts => _attempts;
+        public bool IsGameWon { get; private set; }
+
+        public string MakeGuess(int guess)
+        {
+            if (guess < 1 || guess > 100)
+            {
+                return "Число має бути від 1 до 100!";
+            }
+
+            _attempts++;
+
+            if (guess == _targetNumber)
+            {
+                IsGameWon = true;
+                return $"Вітаємо! Ви вгадали число {_targetNumber} за {_attempts} спроб!";
+            }
+
+            return guess < _targetNumber
+                ? "Загадане число більше!"
+                : "Загадане число менше!";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ласкаво просимо до гри 'Вгадай число'!");
+            Console.WriteLine("Спробуйте вгадати число від 1 до 100.");
+
+            var game = new NumberGame();
+
+            while (!game.IsGameWon)
+            {
+                Console.Write("Введіть ваше число: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out int guess))
+                {
+                    Console.WriteLine("Будь ласка, введіть коректне число!");
+                    continue;
+                }
+
+                string result = game.MakeGuess(guess);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ GuessNumberGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("GuessNumberGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuessNumberGame")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("24e8d22f-ce30-46ad-ad9b-8e509b86ffe2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
